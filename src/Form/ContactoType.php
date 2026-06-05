4a27def92250d897de5296597e0695b24c3827f4
ElvisCutipa <elviscutipariveros@gmail.com>
2026-06-05 18:42:01 -0400

    Symfony Commit 4: Desarrollo del componente FormType y validación de campos del servidor

---
+++ src/Form/ContactoType.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContactoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nombre', TextType::class, ['label' => 'Nombre completo'])
+            ->add('correo', EmailType::class, ['label' => 'Correo electrónico'])
+            ->add('mensaje', TextareaType::class, ['label' => 'Tu mensaje']);
+    }
+}
